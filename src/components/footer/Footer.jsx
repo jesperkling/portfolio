918d91f0b71aa213cb1e8424f1a6ce09ae2f779e
Jesper Kling <jesperkling87@gmail.com>
2024-03-09 09:58:44 +0100

    Created a footer and styled it.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,35 +4,49 @@ import "./footer.scss";
 const Footer = () => {
   return (
     <footer>
-      <ul className="name">
-        <li>&copy; 2024 Jesper Kling</li>
-      </ul>
-      <div className="links">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
+      <div className="footer-wrapper">
+        <ul className="name">
+          <li>&copy; 2024 Jesper Kling</li>
         </ul>
+        <div className="links">
+          <ul>
+            <li>Main</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
 
-        <ul>
-          <li>Contact</li>
-          <li>
-            <a href="https://www.linkedin.com/in/jesper-kling/" target="_blank">
-              LinkedIn
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jesperkling" target="_blank">
-              Github
-            </a>
-          </li>
-        </ul>
+          <ul>
+            <li>Contact</li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/jesper-kling/"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href="https://read.cv/jesperkling" target="_blank">
+                CV
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/jesperkling" target="_blank">
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="mailto:jesperkling87@gmail.com">Mail</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
