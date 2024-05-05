245da2751d130f38f46dd80e7812ca75b25a2d62
Jesper Kling <jesperkling87@gmail.com>
2024-05-05 09:56:32 +0200

    Added light theme and button to change it

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./navbar.scss";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 
 const Navbar = () => {
+  const [isLightTheme, setIsLightTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsLightTheme(!isLightTheme);
+    document.body.classList.toggle("light-theme");
+  };
+
   return (
-    <div className="navbar-container">
+    <div className={`navbar-container ${isLightTheme ? "light-theme" : ""}`}>
       <nav className="navbar">
         <ul>
           <li>
@@ -16,6 +25,9 @@ const Navbar = () => {
             <Link to="/projects">Projects</Link>
           </li>
         </ul>
+        <button onClick={toggleTheme}>
+          {isLightTheme ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}
+        </button>
       </nav>
     </div>
   );
